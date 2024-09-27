b07302e12cfec648fb4f547c122b1c3d692f04d1
Fernando Avellar Jr. <favellar@yahoo.com.br>
2024-09-27 10:57:16 -0300

    add midleware and react redux-devtools

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -2,13 +2,20 @@ const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
+  isLoading: false,
 };
 
 //reducer
 export default function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case "account/deposit":
-      return { ...state, balance: state.balance + action.payload };
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+        isLoading: false,
+      };
+    case "account/convertingCurrency":
+      return { ...state, isLoading: true };
     case "account/withdraw":
       return { ...state, balance: state.balance - action.payload };
     case "account/requestLoan":
@@ -32,8 +39,20 @@ export default function accountReducer(state = initialStateAccount, action) {
 }
 
 //action creators
-export function deposit(amount) {
-  return { type: "account/deposit", payload: amount };
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+
+  //if returns a function redux will pass it to the thunx middleware
+  return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+    );
+    const data = await res.json();
+    const convertedAmount = data.rates.USD;
+
+    dispatch({ type: "account/deposit", payload: convertedAmount });
+  };
 }
 export function withdraw(amount) {
   return { type: "account/withdraw", payload: amount };
--- src/store.js
+++ src/store.js
@@ -1,12 +1,21 @@
-import { combineReducers, legacy_createStore as createStore } from "redux";
+import {
+  applyMiddleware,
+  combineReducers,
+  legacy_createStore as createStore,
+} from "redux";
+import { thunk } from "redux-thunk";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
 const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
